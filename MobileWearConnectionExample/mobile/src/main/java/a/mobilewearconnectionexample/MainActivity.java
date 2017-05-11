e6fc0ec514f45f3b142acbf4cde5b9f4c4b98f2c
Aku <aku.jaakola@luukku.com>
2017-05-11 09:19:24 +0300

    Example with data layer done

---
+++ MobileWearConnectionExample/mobile/src/main/java/a/mobilewearconnectionexample/MainActivity.java
@@ -0,0 +1,110 @@
+package a.mobilewearconnectionexample;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
+
+
+// This application showcases using the data layer in sending data between connected mobile and wearable devices.
+// The mobile screen has an editable textfield and when the button is pressed, the text is displayed on the wear screen.
+
+
+public class MainActivity extends Activity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    String text = "";
+    GoogleApiClient googleClient;
+    EditText edittext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edittext = (EditText) findViewById(R.id.editText);
+
+        //Set up a new GoogleApiClient for the Wearable.API
+        googleClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    // Go here when 'Send' button is pressed
+    public void sendText(View view) {
+        text = edittext.getText().toString();
+        edittext.setText("");
+        new SendToDataLayerThread("/data_path", "" + text).start();
+    }
+
+    // Connect to the datalayer when starting activity
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleClient.connect();
+    }
+
+    // Send a message when the data layer connection is successful
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        String message = "Data layer connection successful";
+        new SendToDataLayerThread("/data_path", message).start();
+    }
+
+    // Stop the data layer connection when activity stops
+    @Override
+    protected void onStop() {
+        if (null != googleClient && googleClient.isConnected()) {
+            googleClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    // Placeholders for required connection callbacks
+    @Override
+    public void onConnectionSuspended(int cause) { }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) { }
+
+
+    class SendToDataLayerThread extends Thread {
+        String path;
+        String message;
+
+        // Constructor which sends the message to the data layer
+        SendToDataLayerThread(String p, String msg) {
+            path = p;
+            message = msg;
+        }
+
+        public void run() {
+            NodeApi.GetConnectedNodesResult nodes = Wearable.NodeApi.getConnectedNodes(googleClient).await();
+            for (Node node : nodes.getNodes()) {
+                MessageApi.SendMessageResult result = Wearable.MessageApi.sendMessage(googleClient, node.getId(), path, message.getBytes()).await();
+                if (result.getStatus().isSuccess()) {
+                    Log.v("myTag", "Message: {" + message + "} sent to: " + node.getDisplayName());
+                }
+                else {
+                    Log.v("myTag", "ERROR: failed to send the message");
+                }
+            }
+        }
+    }
+
+}
+
+
+
---
+++ MobileWearConnectionExample/wear/src/main/java/a/mobilewearconnectionexample/MainActivity.java
@@ -0,0 +1,46 @@
+package a.mobilewearconnectionexample;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView, textview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+                textview = (TextView) stub.findViewById(R.id.msg);
+            }
+        });
+
+        // Register the local broadcast receiver
+        IntentFilter messageFilter = new IntentFilter(Intent.ACTION_SEND);
+        MessageReceiver messageReceiver = new MessageReceiver();
+        LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver, messageFilter);
+
+    }
+
+    public class MessageReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String data = intent.getStringExtra("data");
+            // Display message in UI
+            textview.setText(data);
+        }
+    }
+
+}
