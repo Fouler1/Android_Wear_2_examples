13408b3eaa90bd6b962c6fde7b181846a215b9d9
Aku <aku.jaakola@luukku.com>
2017-06-19 11:55:18 +0300

    Exercise 4 done

---
+++ Exercise_4/app/src/main/java/a/exercise_4/CustomComplicationProviderService.java
@@ -0,0 +1,101 @@
+package a.exercise_4;
+
+import android.icu.text.DateFormat;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
+import android.icu.util.TimeZone;
+import android.support.wearable.complications.ComplicationData;
+import android.support.wearable.complications.ComplicationManager;
+import android.support.wearable.complications.ComplicationProviderService;
+import android.support.wearable.complications.ComplicationText;
+import android.util.Log;
+
+import java.util.Date;
+
+public class CustomComplicationProviderService extends ComplicationProviderService {
+
+    private static final String TAG = "ComplicationProvider";
+
+    /*
+     * Called when a complication has been activated. The method is for any one-time
+     * (per complication) set-up.
+     *
+     * You can continue sending data for the active complicationId until onComplicationDeactivated()
+     * is called.
+     */
+    @Override
+    public void onComplicationActivated(
+            int complicationId, int dataType, ComplicationManager complicationManager) {
+        Log.d(TAG, "onComplicationActivated(): " + complicationId);
+    }
+
+    /*
+     * Called when the complication needs updated data from your provider. There are four scenarios
+     * when this will happen:
+     *
+     *   1. An active watch face complication is changed to use this provider
+     *   2. A complication using this provider becomes active
+     *   3. The period of time you specified in the manifest has elapsed (UPDATE_PERIOD_SECONDS)
+     *   4. You triggered an update from your own class via the
+     *       ProviderUpdateRequester.requestUpdate() method.
+     */
+    @Override
+    public void onComplicationUpdate(
+            int complicationId, int dataType, ComplicationManager complicationManager) {
+        Log.d(TAG, "onComplicationUpdate() id: " + complicationId);
+
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT+2:00"));
+        Date currentLocalTime = cal.getTime();
+        DateFormat date = new SimpleDateFormat("HH:mm");
+        date.setTimeZone(TimeZone.getTimeZone("GMT+2:00"));
+
+        String localTime = date.format(currentLocalTime);
+
+        ComplicationData complicationData = null;
+
+        switch (dataType) {
+            case ComplicationData.TYPE_SHORT_TEXT:
+                complicationData =
+                        new ComplicationData.Builder(ComplicationData.TYPE_SHORT_TEXT)
+                                .setShortText(ComplicationText.plainText(localTime))
+                                .build();
+                break;
+            case ComplicationData.TYPE_LONG_TEXT:
+                complicationData =
+                        new ComplicationData.Builder(ComplicationData.TYPE_LONG_TEXT)
+                                .setLongText(ComplicationText.plainText(localTime))
+                                .build();
+                break;
+            case ComplicationData.TYPE_RANGED_VALUE:
+                complicationData =
+                        new ComplicationData.Builder(ComplicationData.TYPE_RANGED_VALUE)
+                                .setValue(0)
+                                .setMinValue(0)
+                                .setMaxValue(20)
+                                .setShortText(ComplicationText.plainText(localTime))
+                                .build();
+                break;
+            default:
+                if (Log.isLoggable(TAG, Log.WARN)) {
+                    Log.w(TAG, "Unexpected complication type " + dataType);
+                }
+        }
+
+        if (complicationData != null) {
+            complicationManager.updateComplicationData(complicationId, complicationData);
+
+        } else {
+            // If no data is sent, we still need to inform the ComplicationManager, so the update
+            // job can finish and the wake lock isn't held any longer than necessary.
+            complicationManager.noUpdateRequired(complicationId);
+        }
+    }
+
+    /*
+     * Called when the complication has been deactivated.
+     */
+    @Override
+    public void onComplicationDeactivated(int complicationId) {
+        Log.d(TAG, "onComplicationDeactivated(): " + complicationId);
+    }
+}
